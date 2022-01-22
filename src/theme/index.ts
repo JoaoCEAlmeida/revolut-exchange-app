a9248f810bb718211a36aac6027aec24a33f649c
johnnygato <joao88vsc@gmail.com>
2022-01-22 17:08:03 +0000

    create app color theme

---
+++ src/theme/index.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  colors: {
+    primary: {
+      main: '#0666EB'
+    },
+    secondary: {
+      main: '#CCCCCC',
+      contrast: '#EDEEF0'
+    },
+    background: {
+      main: '#fff',
+      transparent: 'transparent'
+    },
+    text: {
+      primary: '#000',
+      secondary: '#fff'
+    },
+    error: {
+      main: '#F5261C'
+    }
+  }
+};
